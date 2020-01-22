dc568a12d6d7eb8a806803643a990f47165f062b
pavikaa <mpavicic99@gmail.com>
2020-01-22 10:17:03 +0100

    Update 3-8.cpp

--- 3-8.cpp
+++ 3-8.cpp
@@ -19,12 +19,12 @@ class Enemy {
 	int mHealth;
 	int mAttackDamage;
 public:
-	Enemy(std::string name,int health, int attackDamage):mName(name),mHealth(health),mAttackDamage(attackDamage){}
+	Enemy(std::string name, int health, int attackDamage) :mName(name), mHealth(health), mAttackDamage(attackDamage) {}
 	int getAttackDamage() const { return mAttackDamage; }
 	int getHealth() const { return mHealth; }
 	std::string getName() const { return mName; }
 	virtual void heal(int amount);
-	virtual void takeDamage(int amount)=0;
+	virtual void takeDamage(int amount) = 0;
 };
 void Enemy::heal(int amount)
 {
@@ -36,8 +36,8 @@ class ShieldedEnemy : public Enemy
 	double mhitProbability;
 	int mshieldHealth;
 public:
-	ShieldedEnemy(std::string name,int health,int attackDamage,double hitProbability,int shieldHealth):Enemy(name,health,attackDamage),mhitProbability(hitProbability),mshieldHealth(shieldHealth){}
-	ShieldedEnemy():Enemy("Default", 100, 50), mhitProbability(0.5), mshieldHealth(100) {}
+	ShieldedEnemy(std::string name, int health, int attackDamage, double hitProbability, int shieldHealth) :Enemy(name, health, attackDamage), mhitProbability(hitProbability), mshieldHealth(shieldHealth) {}
+	ShieldedEnemy() :Enemy("Default", 100, 50), mhitProbability(0.5), mshieldHealth(100) {}
 	int getShieldHealth() const { return mshieldHealth; }
 	double getHitProbability() const { return mhitProbability; }
 	void takeDamage(int);
@@ -49,7 +49,7 @@ void ShieldedEnemy::takeDamage(int amount)
 		return;
 	else
 	{
-		if (mshieldHealth-amount >= 0)
+		if (mshieldHealth - amount >= 0)
 			mshieldHealth -= amount;
 		else if (mshieldHealth - amount < 0)
 		{
@@ -61,37 +61,34 @@ void ShieldedEnemy::takeDamage(int amount)
 }
 class IllegalEnemyException :public std::runtime_error
 {
-	Enemy *enemy;
+	Enemy* enemy;
 public:
-	IllegalEnemyException(std::string message,Enemy *enemy) :std::runtime_error(message),enemy(enemy){}
+	IllegalEnemyException(std::string message, Enemy* enemy) :std::runtime_error(message), enemy(enemy) {}
 	std::string getEnemyName() const { return enemy->getName(); }
 	int getEnemyHealth() const { return enemy->getHealth(); }
 	int getEnemyAttackDamage() const { return enemy->getAttackDamage(); }
 };
 Enemy** createEnemies(const int n)
 {
-	int health = getRandomInt(0, 100);
-	int attackDamage = getRandomInt(0, 100);
-	int shieldHealth = getRandomInt(0, 100);
-	double hitProbability = getRandomDouble(0, 1);
-	Enemy** data=new Enemy*[n];
+	Enemy** data = new Enemy * [n];
 	for (int i = 0; i < n; i++)
 	{
-		health = getRandomInt(0, 100);
-		attackDamage = getRandomInt(0, 100);
-		shieldHealth = getRandomInt(0, 100);
-		data[i] =new ShieldedEnemy("Enemy" + std::to_string(i + 1), health, attackDamage, hitProbability, shieldHealth);
-		try 
+		int health = getRandomInt(0, 100);
+		int attackDamage = getRandomInt(0, 100);
+		int shieldHealth = getRandomInt(0, 100);
+		double hitProbability = getRandomDouble(0, 1);
+		try
 		{
-			if (attackDamage < 50)
+			data[i] = new ShieldedEnemy("Enemy" + std::to_string(i + 1), health, attackDamage, hitProbability, shieldHealth);
+			if (data[i]->getAttackDamage() < 50)
 			{
-				throw(IllegalEnemyException("Attack damage too low.", data[i]));
-				i--;
+				throw(IllegalEnemyException("Attack damage too low", data[i]));
 			}
 		}
 		catch (const IllegalEnemyException & e)
 		{
-			std::cout << e.getEnemyName() << ", " << e.getEnemyHealth() << ", " << e.getEnemyAttackDamage() << std::endl;
+			i--;
+			std::cout << e.what() << ", " << e.getEnemyName() << ", " << e.getEnemyHealth() << ", " << e.getEnemyAttackDamage() << std::endl;
 		}
 	}
 	return data;
@@ -115,6 +112,7 @@ int runBattle(Enemy** enemies1, Enemy** enemies2)
 		if (enemies2[i]->getHealth() > 0)
 			counter2++;
 	}
+	
 	if (counter1 > counter2)
 		return 1;
 	if (counter1 < counter2)
@@ -125,7 +123,14 @@ int runBattle(Enemy** enemies1, Enemy** enemies2)
 int main()
 {
 	srand(time(NULL));
-		Enemy** enemies1 = createEnemies(5);
-		Enemy** enemies2 = createEnemies(5);
-		std::cout << runBattle(enemies1, enemies2);
+	Enemy** enemies1 = createEnemies(5);
+	Enemy** enemies2 = createEnemies(5);
+	std::cout << runBattle(enemies1, enemies2);
+	for (int i = 0; i < 5; i++)
+	{
+		delete enemies1[i];
+		delete enemies2[i];
+	}
+	delete enemies1;
+	delete enemies2;
 }
